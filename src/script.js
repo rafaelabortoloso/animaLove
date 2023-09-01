65763be79dd97328fae745b405dc2639a2ff04f7
rafaelabortoloso <rbortoloso9@gmail.com>
2023-09-01 12:12:00 -0300

    aplicando layout em grid e segunda tela

--- src/script.js
+++ src/script.js
@@ -18,7 +18,9 @@ document.addEventListener("DOMContentLoaded", function () {
     });
 });
 
-const searchInput = document.getElementById('input');
+///////////////////////////////////////////////////////////////////////////////////////////
+
+const searchInput = document.getElementById('search-input');
 
 searchInput.addEventListener('focus', () => {
     searchInput.removeAttribute('placeholder');
@@ -30,11 +32,47 @@ searchInput.addEventListener('blur', () => {
     }
 });
 
+const nameInput = document.getElementById('name-input');
+const emailInput = document.getElementById('email-input');
+const messageInput = document.getElementById('message-input');
+
+nameInput.addEventListener('focus', () => {
+    nameInput.removeAttribute('placeholder');
+});
+
+nameInput.addEventListener('blur', () => {
+    if (!nameInput.value) {
+        nameInput.setAttribute('placeholder', 'Nome:');
+    }
+});
+
+emailInput.addEventListener('focus', () => {
+    emailInput.removeAttribute('placeholder');
+});
+
+emailInput.addEventListener('blur', () => {
+    if (!emailInput.value) {
+        emailInput.setAttribute('placeholder', 'E-mail:');
+    }
+});
+
+messageInput.addEventListener('focus', () => {
+    messageInput.removeAttribute('placeholder');
+});
+
+messageInput.addEventListener('blur', () => {
+    if (!messageInput.value) {
+        messageInput.setAttribute('placeholder', 'Mensagem:');
+    }
+});
+
+///////////////////////////////////////////////////////////////////////////////////////////
+
 const carousel = document.getElementById('carousel');
 const prevButton = document.getElementById('prev-btn');
 const nextButton = document.getElementById('next-btn');
-const cardWidth = 220; // Largura do card + margem
-const totalCards = carousel.children.length; // Não é necessário dividir por 2
+const cardWidth = 310; // Largura do card + margem
+const totalCards = carousel.children.length;
 let currentPosition = 0;
 
 prevButton.addEventListener('click', () => {
@@ -46,4 +84,6 @@ nextButton.addEventListener('click', () => {
     currentPosition = (currentPosition + 1) % totalCards;
     carousel.style.transform = `translateX(-${currentPosition * cardWidth}px)`;
 });
+
+/////////////////////////////////////////////////////////////////////////////////////////////////
 
